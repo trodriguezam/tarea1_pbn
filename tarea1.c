1662548aff7022160c168d544dbb29f61e9ba1f8
trodriguezam <torotegui@gmail.com>
2023-04-07 14:14:51 -0400

    feat: added teams coordinates list (printeo todo para que se pueda ir viendo que hace el programa)

--- tarea1.c
+++ tarea1.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+int distance(int x1, int x2, int y1, int y2){
+    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
 
 int main(){
 	char line[100];
-    int teams[10], k = 0;
+    float teams[20][2];
+    int k = 0;
 	FILE* input;
+    FILE* output;
 	
 	/* input = fopen("entrada.txt", "r");
 	salida = fopen("salida.txt", "w");
 	fgets(linea, 199, input);
     fprintf(salida, "%s", linea); */
-
-    input = fopen("equipos.txt", "r");
+    // printf("test1 ");
+    input = fopen("equipos_corto.txt", "r");
+    output = fopen("partidos.txt", "w");
+    // printf("test2 ");
     
     while(fgets(line, 99, input)){
-        char* value = strtok(line, ", ");
-        int teamx[3];
+        // printf("test3 ");
+        char* value = strtok(line, ",");
+        char* teamx;
+        float x;
+        float y;
         for(int i = 0; i<3; i++){
+            // printf("test4 ");
             // printf("%s", value);
-            if (i == (int)0){
-                teamx[i] = value[i];
-                printf("%s", teamx[i]);
+            if (i == 0){
+                // printf("test5 ");
+                // printf("%s\n", value);
+                teamx = value;
+                printf("%s\n", teamx);
             }
-            else{
-                teamx[i] = value[i];
-                printf("%d", (int)teamx[i]);
+            else if(i == 1){
+                // printf("test6 ");
+                x = atof(value);
+                printf("%f\n", x);
             }
-            value = strtok(NULL, ", ");
+            else if(i == 2){
+                // printf("test6 ");
+                y = atof(value);
+                printf("%f\n", y);
+            }
+            value = strtok(NULL, ",");
         }
-        teams[k] = teamx[k];
+        teams[k][0] = x;
+        teams[k][1] = y;
+        // teams[k] = teamx[k];
         k++;
     }
-
-	printf("%d", &teams[0]);
-	printf("\n");
+    fclose(input);
+    fclose(output);
+    for(int j = 0; j<20; j++){
+        for(int i = 0; i<2; i++){
+            printf("%f, ", teams[j][i]);
+        }
+        printf("\n");
+    }
 	return 0;
 }
